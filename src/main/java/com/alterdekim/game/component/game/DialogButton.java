06679f113639aa39b42d7caee5c6b8f1c6acd1bd
alterdekim <alterwain@protonmail.com>
2024-07-08 22:55:40 +0300

    objectmapper patch x3

--- src/main/java/com/alterdekim/game/component/game/DialogButton.java
+++ src/main/java/com/alterdekim/game/component/game/DialogButton.java
@@ -2,10 +2,12 @@ package com.alterdekim.game.component.game;
 
 import com.alterdekim.game.websocket.message.WebSocketMessageType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Getter
 public class DialogButton {
     private String buttonText;
     private DialogButtonColor buttonColor;
